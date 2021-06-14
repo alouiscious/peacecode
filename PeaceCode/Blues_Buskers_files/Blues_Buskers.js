6961406a2e7144dd5949b11c27b2dae5ebbcfd2d
Alouiscious <46434917+alouiscious@users.noreply.github.com>
2021-06-13 19:11:23 -0700

    Add files via upload

---
+++ PeaceCode/Blues_Buskers_files/Blues_Buskers.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20120324
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Blues_Buskers_files/Blues_BuskersMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');detectBrowser();adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupAllIEPNGBGs();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ PeaceCode/Contact_RHBS_files/Contact_RHBS.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20120324
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Contact_RHBS_files/Contact_RHBSMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');Widget.onload();fixAllIEPNGs('Media/transparent.gif');IMpreload('Contact_RHBS_files','shapeimage_3','0');IMpreload('Contact_RHBS_files','shapeimage_3','1');IMpreload('Contact_RHBS_files','shapeimage_3','2');IMpreload('Contact_RHBS_files','shapeimage_3','3');IMpreload('Contact_RHBS_files','shapeimage_3','4');fixupIECSS3Opacity('id5');fixupIECSS3Opacity('id6');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
